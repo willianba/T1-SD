9bc59aa55b1d49015adc749696e044e258ac4765
Willian Alves <wba.alves@gmail.com>
2019-04-06 20:04:02 -0300

    create query methods for client and server

--- program.py
+++ program.py
@@ -5,11 +5,25 @@ import sys
 import os
 import json
 
+resources = {}
+condition = threading.Condition()
+
 
 # this is the default value for the switch statement below
 def service_undefined():
     print("Service undefined.")
     sys.exit(-1)
+
+
+def get_all_files():
+    global resources
+    condition.acquire()
+    result = {'files': []}
+    for nested_list in list(resources.values()):
+        for key_list in nested_list:
+            result['files'].append(key_list)
+    condition.release()
+    return result
 
 
 class Thread(threading.Thread):
@@ -20,7 +34,6 @@ class Thread(threading.Thread):
         self.port = port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.server_address = (self.host, self.port)
-        self.resources = {}
 
     def run(self):
         self.select_server_or_client()
@@ -28,23 +41,23 @@ class Thread(threading.Thread):
     # since python doesn't have a switch case statement, this is a way to implement it
     def server_switch(self, service):
         return {
-            Service.SIGN_UP.value: self.server_sign_up
+            Service.SIGN_UP.value: self.server_sign_up,
+            Service.QUERY.value: self.server_query_files
         }.get(service, service_undefined)
 
     # since python doesn't have a switch case statement, this is a way to implement it
     def client_switch(self, service):
         return {
-            Service.SIGN_UP.value: self.client_sign_up
+            Service.SIGN_UP.value: self.client_sign_up,
+            Service.QUERY.value: self.client_query_files
         }.get(service, service_undefined)
 
     def select_server_or_client(self):
         if self.client is False:
-            print("Running server.")
             self.sock.bind(self.server_address)
             while True:
                 self.run_service(self.port)
         else:
-            print("Running client.")
             self.execute_service(self.port)
 
     def run_service(self, port):
@@ -72,7 +85,6 @@ class Thread(threading.Thread):
             self.sock.close()
 
     def server_sign_up(self):
-        print('Waiting for client connection')
         data, address = self.sock.recvfrom(4096)
         client_ip = address[0]
         data = json.loads(data)
@@ -80,5 +92,23 @@ class Thread(threading.Thread):
         print(f"Client {client_ip} connected and sent {len(files)} files")
         if data:
             print('Confirming connection')
-            self.resources[client_ip] = files
+            global resources
+            condition.acquire()
+            resources[client_ip] = files
+            condition.release()
             self.sock.sendto(b"Connected", address)
+
+    def client_query_files(self):
+        self.sock.sendto(b"query", self.server_address)
+        print("Waiting files from server")
+        data = self.sock.recv(4096)
+        data = json.loads(data)
+        files = data['files']
+        print(f"These are the available files: {', '.join([str(x) for x in files])}")
+
+    def server_query_files(self):
+        data, address = self.sock.recvfrom(4096)
+        client_ip = address[0]
+        print(f"Sending {client_ip} a list of available files")
+        files = get_all_files()
+        self.sock.sendto(json.dumps(files).encode(), address)
