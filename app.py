9db407e2a013d9032330ab9b6b18f48faf218f39
Willian Alves <wba.alves@gmail.com>
2019-04-06 20:03:25 -0300

    change how server and clients are created

--- app.py
+++ app.py
@@ -3,13 +3,28 @@ from program import Thread
 from services import Service
 
 
-def start_program(args):
-    if args.client:
-        if args.signup:
-            Thread(Service.SIGN_UP.value, args.client[0], client=True).start()
-    elif args.server:
-        if args.signup:
-            Thread(Service.SIGN_UP.value).start()
+def start_server():
+    print("Running server.")
+    threads = [
+        Thread(Service.SIGN_UP.value),
+        Thread(Service.QUERY.value)
+    ]
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+
+
+def start_client(args):
+    print("Running client.")
+    thread = None
+    server_ip = args.client[0]
+    if args.signup:
+        thread = Thread(Service.SIGN_UP.value, server_ip, client=True)
+    elif args.query:
+        thread = Thread(Service.QUERY.value, server_ip, client=True)
+    thread.start()
+    thread.join()
 
 
 def main():
@@ -18,10 +33,15 @@ def main():
                     help="Start program as client. Need to specify host and service with another flag.")
     ap.add_argument("-s", "--server", action='store_true',
                     help="Start program as server. Need to specify service with another flag.")
-    ap.add_argument("--signup", action='store_true', help="Start sign up service.")
+    ap.add_argument("--signup", action='store_true', help="Execute sign up service.")
+    ap.add_argument("--query", action='store_true', help="Execute query service.")
     args = ap.parse_args()
-    start_program(args)
+    if args.client:
+        start_client(args)
+    elif args.server:
+        start_server()
 
 
 if __name__ == "__main__":
     main()
+
