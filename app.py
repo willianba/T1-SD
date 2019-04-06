5131fcce6f7d1e43529d86f6e12f68764a9d485d
Willian Alves <wba.alves@gmail.com>
2019-04-06 15:45:25 -0300

    change behavior of app to create threads based on boolean arguments and using enums

--- app.py
+++ app.py
@@ -1,24 +1,24 @@
-from program import Thread
-import sys
 import argparse
+from program import Thread
+from services import Service
 
 
 def start_program(args):
     if args.client:
-        Thread(args.client[0], args.client[1], client=True).start()
+        if args.signup:
+            Thread(Service.SIGN_UP.value, args.client[0], client=True).start()
     elif args.server:
-        Thread(args.server[0], args.server[1]).start()
-    else:
-        print("Unknown error.")
-        sys.exit(-1)
+        if args.signup:
+            Thread(Service.SIGN_UP.value).start()
 
 
 def main():
     ap = argparse.ArgumentParser(description="Client Server / P2P", prog="app")
-    ap.add_argument("-c", "--client", metavar=("SERVER", "PORT"), nargs=2,
-                    help="Start program as client. Need to specify host, port, and folder with resources.")
-    ap.add_argument("-s", "--server", metavar=("SERVER", "PORT"), nargs=2,
-                    help="Start program as server. Need to specify port.")
+    ap.add_argument("-c", "--client", metavar="SERVER", nargs=1,
+                    help="Start program as client. Need to specify host and service with another flag.")
+    ap.add_argument("-s", "--server", action='store_true',
+                    help="Start program as server. Need to specify service with another flag.")
+    ap.add_argument("--signup", action='store_true', help="Start sign up service.")
     args = ap.parse_args()
     start_program(args)
 
