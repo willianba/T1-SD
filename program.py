3bb2bd2c36870d3501584e9c4dd095d8a88243f1
Willian Alves <wba.alves@gmail.com>
2019-04-06 23:12:02 -0300

    refactor and reorganized code

--- program.py
+++ program.py
@@ -38,77 +38,82 @@ class Thread(threading.Thread):
     def run(self):
         self.select_server_or_client()
 
-    # since python doesn't have a switch case statement, this is a way to implement it
-    def server_switch(self, service):
-        return {
-            Service.SIGN_UP.value: self.server_sign_up,
-            Service.QUERY.value: self.server_query_files
-        }.get(service, service_undefined)
-
-    # since python doesn't have a switch case statement, this is a way to implement it
-    def client_switch(self, service):
-        return {
-            Service.SIGN_UP.value: self.client_sign_up,
-            Service.QUERY.value: self.client_query_files
-        }.get(service, service_undefined)
-
     def select_server_or_client(self):
-        if self.client is False:
+        if not self.client:
             self.sock.bind(self.server_address)
-            while True:
-                self.run_service(self.port)
+        self.execute_service()()
+
+    # since python doesn't have a switch case statement, this is a way to implement it
+    def execute_service(self):
+        if self.client:
+            return {
+                Service.SIGN_UP.value: self.client_sign_up,
+                Service.QUERY.value: self.client_query_files
+            }.get(self.port, service_undefined)
         else:
-            self.execute_service(self.port)
+            return {
+                Service.SIGN_UP.value: self.server_sign_up,
+                Service.QUERY.value: self.server_query_files
+            }.get(self.port, service_undefined)
 
-    def run_service(self, port):
-        self.server_switch(port)()
-
-    def execute_service(self, port):
-        self.client_switch(port)()
-
-    def client_sign_up(self):
-        folder = input("Specify folder to share files: ")
+    def execute_client_func(self, func, folder=None):
         try:
-            files = os.listdir(folder)
-            data = {'files': files}
-            self.sock.sendto(json.dumps(data).encode(), self.server_address)
-            print('Waiting server response')
-            data, server = self.sock.recvfrom(4096)
-            print(f'Received: {data.decode()}')
+            if folder:
+                func(folder)
+            else:
+                func()
         except FileNotFoundError:
             print("This folder doesn't exist.")
             self.client_sign_up()
+        except ConnectionResetError:
+            print("There are no running hosts.")
+            sys.exit(-2)
         except socket.gaierror:
             print("Host unavailable.")
             sys.exit(-2)
         finally:
             self.sock.close()
 
+    def sign_up(self, folder):
+        files = os.listdir(folder)
+        data = {'files': files}
+        self.sock.sendto(json.dumps(data).encode(), self.server_address)
+        print('Waiting server response.')
+        data, server = self.sock.recvfrom(4096)
+        print(f'Received: {data.decode()}')
+
+    def query(self):
+        self.sock.sendto(b"query", self.server_address)
+        print("Waiting files from server.")
+        data = self.sock.recv(4096)
+        data = json.loads(data)
+        files = data['files']
+        print(f"These are the available files: {', '.join([str(x) for x in files])}")
+
+    def client_sign_up(self):
+        folder = input("Specify folder to share files: ")
+        self.execute_client_func(self.sign_up, folder)
+
+    def client_query_files(self):
+        self.execute_client_func(self.query)
+
     def server_sign_up(self):
         data, address = self.sock.recvfrom(4096)
         client_ip = address[0]
         data = json.loads(data)
         files = data['files']
-        print(f"Client {client_ip} connected and sent {len(files)} files")
+        print(f"Client {client_ip} connected and sent {len(files)} files.")
         if data:
-            print('Confirming connection')
+            print('Confirming connection.')
             semaphore.acquire()
             global resources
             resources[client_ip] = files
             semaphore.release()
             self.sock.sendto(b"Connected", address)
 
-    def client_query_files(self):
-        self.sock.sendto(b"query", self.server_address)
-        print("Waiting files from server")
-        data = self.sock.recv(4096)
-        data = json.loads(data)
-        files = data['files']
-        print(f"These are the available files: {', '.join([str(x) for x in files])}")
-
     def server_query_files(self):
         data, address = self.sock.recvfrom(4096)
         client_ip = address[0]
-        print(f"Sending {client_ip} a list of available files")
+        print(f"Sending {client_ip} a list of available files.")
         files = get_all_files()
         self.sock.sendto(json.dumps(files).encode(), address)
