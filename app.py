17ea144790685753f085a895a38db7984125d74d
Willian Alves <wba.alves@gmail.com>
2019-04-07 01:43:53 -0300

    handle an invalid option and rewrite some help text

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import argparse
+import sys
 from program import Thread
 from services import Service
 
@@ -30,16 +31,20 @@ def start_client(args):
 def main():
     ap = argparse.ArgumentParser(description="Client Server / P2P", prog="app")
     ap.add_argument("-c", "--client", metavar="SERVER", nargs=1,
-                    help="Start program as client. Need to specify host and service with another flag.")
+                    help="Start program as client. Need to specify host as parameter and service with another flag.")
     ap.add_argument("-s", "--server", action='store_true',
-                    help="Start program as server. Need to specify service with another flag.")
-    ap.add_argument("--signup", action='store_true', help="Execute sign up service.")
-    ap.add_argument("--query", action='store_true', help="Execute query service.")
+                    help="Start program as server. Run all services in different threads.")
+    ap.add_argument("-su", "--sign_up", action='store_true', help="Execute sign up service.")
+    ap.add_argument("-q", "--query", action='store_true', help="Execute query service.")
+    ap.add_argument("-hb", "--heartbeat", action='store_true', help="Execute heartbeat service.")
     args = ap.parse_args()
     if args.client:
         start_client(args)
     elif args.server:
         start_server()
+    else:
+        print("Need to start server or run a client with a service.")
+        sys.exit(0)
 
 
 if __name__ == "__main__":
